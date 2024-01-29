dd13f5e9c5d318da17b5856425d1e26afc7b638c
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-01-29 17:22:21 +0530

    added rspec for command turn right

--- spacecraft_spec.rb
+++ spacecraft_spec.rb
@@ -141,3 +141,63 @@ describe '#execute' do
     end
   end
 end
+
+describe '#turn' do
+  context 'turn right' do
+    it 'turns spacecraft to the right from north' do
+      spacecraft = Spacecraft.new(0, 0, 0, 'W', 'N', 'E')
+
+      commands = ['r']
+
+      spacecraft.execute(commands)
+
+      expected_position = [0, 0, 0]
+      expected_direction = 'E'
+
+      expect(spacecraft.position).to eq(expected_position)
+      expect(spacecraft.direction[:front]).to eq(expected_direction)
+    end
+
+    it 'turns spacecraft to the right from north' do
+      spacecraft = Spacecraft.new(0, 0, 0, 'N', 'E', 'S')
+
+      commands = ['r']
+
+      spacecraft.execute(commands)
+
+      expected_position = [0, 0, 0]
+      expected_direction = 'S'
+
+      expect(spacecraft.position).to eq(expected_position)
+      expect(spacecraft.direction[:front]).to eq(expected_direction)
+    end
+
+    it 'turns spacecraft to the right from north' do
+      spacecraft = Spacecraft.new(0, 0, 0, 'E', 'S', 'W')
+
+      commands = ['r']
+
+      spacecraft.execute(commands)
+
+      expected_position = [0, 0, 0]
+      expected_direction = 'W'
+
+      expect(spacecraft.position).to eq(expected_position)
+      expect(spacecraft.direction[:front]).to eq(expected_direction)
+    end
+
+    it 'turns spacecraft to the right from north' do
+      spacecraft = Spacecraft.new(0, 0, 0, 'S', 'W', 'N')
+
+      commands = ['r']
+
+      spacecraft.execute(commands)
+
+      expected_position = [0, 0, 0]
+      expected_direction = 'N'
+
+      expect(spacecraft.position).to eq(expected_position)
+      expect(spacecraft.direction[:front]).to eq(expected_direction)
+    end
+  end
+end
