61ef341869cbca9f7552745845aea40b37c42392
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-01-29 15:38:50 +0530

    implementation of initialization

--- spacecraft.rb
+++ spacecraft.rb
@@ -1,4 +1,10 @@
 # lib/spacecraft.rb
 
 class Spacecraft
+  attr_reader :position, :direction
+
+  def initialize(x, y, z, l, f, r)
+    @position = [x, y, z]
+    @direction = { left: l, front: f, right: r }
+  end
 end
--- spacecraft_spec.rb
+++ spacecraft_spec.rb
@@ -2,8 +2,8 @@ require_relative 'spacecraft'
 
 describe Spacecraft do
   it 'can be created with a starting point and direction' do
-    spacecraft = Spacecraft.new(0, 0, 0, 'l', 'f', 'r' )
+    spacecraft = Spacecraft.new(0, 0, 0, :l, :f, :r)
     expect(spacecraft.position).to eq([0, 0, 0])
-    expect(spacecraft.direction).to eq('f')
+    expect(spacecraft.direction).to eq({ left: :l, front: :f, right: :r })
   end
 end
