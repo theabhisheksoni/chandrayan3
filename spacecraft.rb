b46e823f9cee6eaedb12f22fb1257c1770c74ea7
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-01-29 16:47:59 +0530

    implementation of moving forward to all directions

--- spacecraft.rb
+++ spacecraft.rb
@@ -7,4 +7,40 @@ class Spacecraft
     @position = [x, y, z]
     @direction = { left: l, front: f, right: r }
   end
+
+  def execute(commands)
+    commands.each do |command|
+      process(command)
+    end
+    x, y, z = @position
+    puts "Final Position: (#{x}, #{y}, #{z})"
+    puts "Final Direction: #{@direction[:front]}"
+  end
+
+  def process(command)
+    case command
+    when 'f'
+      move(command)
+    end
+  end
+
+  def move(command)
+    case command
+    when 'f'
+      case @direction[:front]
+      when 'N'
+        @position[1] += 1
+      when 'S'
+        @position[1] -= 1
+      when 'E'
+        @position[0] += 1
+      when 'W'
+        @position[0] -= 1
+      when 'U'
+        @position[2] += 1
+      when 'D'
+        @position[2] -= 1
+      end
+    end
+  end
 end
