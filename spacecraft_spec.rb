9cc73863ed3e400e35470667ca64ae76cf9e3520
Abhishek Soni <abhisheksoni.coder@gmail.com>
2024-01-29 16:46:10 +0530

    add rspec for forward command for all directions

--- spacecraft_spec.rb
+++ spacecraft_spec.rb
@@ -22,4 +22,54 @@ describe '#execute' do
     expect(spacecraft.position).to eq(expected_position)
     expect(spacecraft.direction[:front]).to eq(expected_direction)
   end
+
+  it 'moves spacecraft forward to east' do
+    spacecraft = Spacecraft.new(0, 0, 0, 'l', 'E', 'r')
+    commands = ['f']
+    spacecraft.execute(commands)
+    expected_position = [1, 0, 0]
+    expected_direction = 'E'
+    expect(spacecraft.position).to eq(expected_position)
+    expect(spacecraft.direction[:front]).to eq(expected_direction)
+  end
+
+  it 'moves spacecraft forward to south' do
+    spacecraft = Spacecraft.new(0, 0, 0, 'l', 'S', 'r')
+    commands = ['f']
+    spacecraft.execute(commands)
+    expected_position = [0, -1, 0]
+    expected_direction = 'S'
+    expect(spacecraft.position).to eq(expected_position)
+    expect(spacecraft.direction[:front]).to eq(expected_direction)
+  end
+
+  it 'moves spacecraft westward' do
+    spacecraft = Spacecraft.new(0, 0, 0, 'l', 'W', 'r')
+    commands = ['f']
+    spacecraft.execute(commands)
+    expected_position = [-1, 0, 0]
+    expected_direction = 'W'
+    expect(spacecraft.position).to eq(expected_position)
+    expect(spacecraft.direction[:front]).to eq(expected_direction)
+  end
+
+  it 'moves spacecraft upward' do
+    spacecraft = Spacecraft.new(0, 0, 0, 'l', 'U', 'r')
+    commands = ['f']
+    spacecraft.execute(commands)
+    expected_position = [0, 0, 1]
+    expected_direction = 'U'
+    expect(spacecraft.position).to eq(expected_position)
+    expect(spacecraft.direction[:front]).to eq(expected_direction)
+  end
+
+  it 'moves spacecraft downward' do
+    spacecraft = Spacecraft.new(0, 0, 0, 'l', 'D', 'r')
+    commands = ['f']
+    spacecraft.execute(commands)
+    expected_position = [0, 0, -1]
+    expected_direction = 'D'
+    expect(spacecraft.position).to eq(expected_position)
+    expect(spacecraft.direction[:front]).to eq(expected_direction)
+  end
 end
